91b3fe356d98c2bfaa47a35552e985160bc04276
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-09-08 23:06:34 +0100

    updating UI

--- services.php
+++ services.php
@@ -24,6 +24,222 @@
             </div>
         </div>
     </section>
+    <section class="section-service-intro">
+        <div class="container">
+            <div class="service-intro">
+                <h1 class="service-intro__header">
+                    What we do
+                </h1>
+                <p class="service-intro__paragraph">
+                    Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+                </p>
+                <div class="service-intro__wrapper">
+                    <div class="service-intro__card">
+                        <h1 class="service-intro__heading service-intro__heading--1">
+                            <span class="service-intro__text">
+                                15
+                            </span>
+                            <span class="service-intro__icon">
+                                +
+                            </span>
+                        </h1>
+                        <p class="service-intro__title">
+                            Nobel prizes
+                        </p>
+                    </div>
+                    <div class="service-intro__card">
+                        <h1 class="service-intro__heading service-intro__heading--2">
+                            <span class="service-intro__text">
+                                33
+                            </span>
+                            <span class="service-intro__icon">
+                                +
+                            </span>
+                        </h1>
+                        <p class="service-intro__title">
+                            Nobel prizes
+                        </p>
+                    </div>
+                    <div class="service-intro__card">
+                        <h1 class="service-intro__heading service-intro__heading--3">
+                            <span class="service-intro__text">
+                                12
+                            </span>
+                            <span class="service-intro__icon">
+                                +
+                            </span>
+                        </h1>
+                        <p class="service-intro__title">
+                            Nobel prizes
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-our-services">
+        <div class="container">
+            <div class="our-services">
+                <div class="our-services__wrapper">
+                    <h1 class="our-services__header">
+                        We are Amazing
+                    </h1>
+                    <h1 class="our-services__banner">
+                        Our services
+                    </h1>
+                    <p class="our-services__paragraph">
+                        Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus urna. Quam elementum pulvinar etiam non. Eu consequat.
+                    </p>
+                    <div class="our-services__list">
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--1">
+                                <img src="assets/icons/axe.svg" alt="Chopping heads" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--1">
+                                Chopping heads
+                            </h1>
+                            <p class="our-services__info">
+                                Tincidunt lobortis feugiat vivamus at augue eget. Risus feugiat in ante metus dictum. Amet aliquam id diam maecenas.
+                            </p>
+                        </div>
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--2">
+                                <img src="assets/icons/planetary-pink.svg" alt="Atom dev" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--2">
+                                Atom dev
+                            </h1>
+                            <p class="our-services__info">
+                                Eget nulla facilisi etiam dignissim diam quis enim lobortis scelerisque. Eu feugiat pretium nibh ipsum consequat nisl vel pretium. 
+                            </p>
+                        </div>
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--3">
+                                <img src="assets/icons/tower.svg" alt="Do you even try?" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--3">
+                                Do you even try?
+                            </h1>
+                            <p class="our-services__info">
+                                Tincidunt lobortis feugiat vivamus at augue eget. Risus feugiat in ante metus dictum. Amet aliquam id diam maecenas.
+                            </p>
+                        </div>
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--4">
+                                <img src="assets/icons/scale.svg" alt="Chopping heads" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--4">
+                                Justice for Metallica
+                            </h1>
+                            <p class="our-services__info">
+                                Id venenatis a condimentum vitae. Magna fermentum iaculis eu non diam phasellus vestibulum lorem. Tortor dignissim convallis aenean et.
+                            </p>
+                        </div>
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--5">
+                                <img src="assets/icons/bowling-ball.svg" alt="Better than the Dude" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--5">
+                                Better than the Dude
+                            </h1>
+                            <p class="our-services__info">
+                                Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Malesuada fames ac turpis egestas sed.
+                            </p>
+                        </div>
+                        <div class="our-services__item">
+                            <div class="our-services__icon our-services__icon--6">
+                                <img src="assets/icons/electro-cloud.svg" alt="Hmmmmmmmmmmmmm" class="our-services__image">
+                            </div>
+                            <h1 class="our-services__title our-services__title--6">
+                                Hmmmmmmmmmmmmm
+                            </h1>
+                            <p class="our-services__info">
+                                Integer quis auctor elit sed vulputate mi. Semper auctor neque vitae tempus. Vulputate odio ut enim blandit volutpat maecenas volutpat.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-services-info">
+        <div class="container" >
+            <div class="services-info">
+                <h1 class="services-info__header">
+                    What we do
+                </h1>
+                <h1 class="services-info__banner">
+                    What we  do
+                </h1>
+                <p class="services-info__paragraph">
+                    Upper prior hundred links approach reedy, was to the than and the ever somehow surprised known for every by of there until road, 
+                </p>
+            </div>
+        </div>
+        <div class="services-info__wrapper">
+            <div class="services-info__card services-info__card--1">
+                <div class="services-info__text services-info__text--1">
+                    <h3 class="services-info__number services-info__number--1">
+                        01
+                    </h3>
+                    <h1 class="services-info__heading">
+                        Upper prior hundred links approach reedy
+                    </h1>
+                    <p class="services-info__detail">
+                        Upper prior hundred links approach reedy, was to the than and the ever somehow surprised known for every by of there until road, 
+                    </p>
+                </div>
+                <div class="services-info__image-box services-info__image-box--1">
+                    <img src="assets/images/photo--1.png" alt="photo" class="services-info__image">
+                </div>
+                <div class="services-info__overlay services-info__overlay--1">&nbsp;</div>
+            </div>
+            <div class="services-info__card services-info__card--2">
+                <div class="services-info__image-box services-info__image-box--2">
+                    <img src="assets/images/photo--2.png" alt="photo" class="services-info__image">
+                </div>
+                <div class="services-info__overlay services-info__overlay--2">&nbsp;</div>
+                <div class="services-info__text services-info__text--2">
+                    <h3 class="services-info__number services-info__number--2">
+                        02
+                    </h3>
+                    <h1 class="services-info__heading">
+                        Upper prior hundred links approach reedy
+                    </h1>
+                    <p class="services-info__detail">
+                        Upper prior hundred links approach reedy, was to the than and the ever somehow surprised known for every by of there until road, 
+                    </p>
+                </div>
+            </div>
+            <div class="services-info__card services-info__card--3">
+                <div class="services-info__text services-info__text--3">
+                    <h3 class="services-info__number services-info__number--3">
+                        03
+                    </h3>
+                    <h1 class="services-info__heading">
+                        Upper prior hundred links approach reedy
+                    </h1>
+                    <p class="services-info__detail">
+                        Upper prior hundred links approach reedy, was to the than and the ever somehow surprised known for every by of there until road, 
+                    </p>
+                </div>
+                <div class="services-info__image-box services-info__image-box--3">
+                    <img src="assets/images/photo--3.png" alt="photo" class="services-info__image">
+                </div>
+                <div class="services-info__overlay services-info__overlay--3">&nbsp;</div>
+            </div>
+        </div>
+    </section>
+    <script src="assets/Js/jquery.min.js"></script>
+    <script src="assets/Js/jquery.waypoints.min.js"></script>
+    <script src="assets/Js/jquery.rcounter.js"></script>
+    <script>
+        $(function(){
+            $('.service-intro__text').rCounter({
+                duration: 20
+            });
+        });
+    </script>
 <?php
     // include "includes/testimonial.php";
     include "includes/newsletter.php";
