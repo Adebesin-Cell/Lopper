e6a8c96a9bcd0bc6150f89ee282f5f11ee943335
Adebesin-Cell <adebesintolulope80@gmail.com>
2021-09-15 14:19:19 +0100

    updating UI

--- includes/testimonial.php
+++ includes/testimonial.php
@@ -49,6 +49,72 @@
                         </div>
                     </div>
                 </div>
+                <div class="item">
+                    <div class="testimonial__item">
+                        <div class="testimonial__image-box">
+                            <img src="assets/images/slider--2.png" alt="slider" class="testimonial__image">
+                        </div>
+                        <div class="testimonial__body">
+                            <p class="testimonial__text">
+                                Lectus arcu bibendum at varius. Adipiscing diam donec adipiscing tristique.
+                            </p>
+
+                            <p class="testimonial__info">
+                                <span class="testimonial__name">
+                                    Kerry Johnes 
+                                </span>
+                                    -
+                                <span class="testimonial__company">
+                                    Some Company
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimonial__item">
+                        <div class="testimonial__image-box">
+                            <img src="assets/images/slider--3.png" alt="slider" class="testimonial__image">
+                        </div>
+                        <div class="testimonial__body">
+                            <p class="testimonial__text">
+                                Lectus arcu bibendum at varius. Adipiscing diam donec adipiscing tristique.
+                            </p>
+
+                            <p class="testimonial__info">
+                                <span class="testimonial__name">
+                                    Kerry Johnes 
+                                </span>
+                                    -
+                                <span class="testimonial__company">
+                                    Some Company
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="item">
+                    <div class="testimonial__item">
+                        <div class="testimonial__image-box">
+                            <img src="assets/images/slider--4.png" alt="slider" class="testimonial__image">
+                        </div>
+                        <div class="testimonial__body">
+                            <p class="testimonial__text">
+                                Lectus arcu bibendum at varius. Adipiscing diam donec adipiscing tristique.
+                            </p>
+
+                            <p class="testimonial__info">
+                                <span class="testimonial__name">
+                                    Kerry Johnes 
+                                </span>
+                                    -
+                                <span class="testimonial__company">
+                                    Some Company
+                                </span>
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
